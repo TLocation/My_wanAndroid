354d55efdf3ffae9eb58a3e4c9d59a965d36c8d3
FromChainStudent <1379587022@qq.com>
2018-04-04 20:29:24 +0800

    空页面

---
+++ app/src/main/java/com/txl/wanandroid/my_wanandroid/activity/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.txl.wanandroid.my_wanandroid.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.txl.wanandroid.my_wanandroid.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
