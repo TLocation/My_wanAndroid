a53b62c3329430d96cad716bef1d54e2dd6f5b7a
axi0712 <1255645340@qq.com>
2018-04-06 20:12:10 +0800

    增加俩工具类

---
+++ app/src/main/java/com/txl/wanandroid/my_wanandroid/utils/KeyUtils.kt
@@ -0,0 +1,16 @@
+package com.txl.wanandroid.my_wanandroid.utils
+
+/**
+ *
+ * 项目名称: 药到家
+ * 类描述:
+ * 创建人: 田晓龙
+ * 创建时间: 2018/4/6 0006 19:52
+ * 修改人:
+ * 修改内容:
+ * 修改时间:
+ */
+
+
+object KeyUtils {
+}
---
+++ app/src/main/java/com/txl/wanandroid/my_wanandroid/utils/UrlUtils.kt
@@ -0,0 +1,16 @@
+package com.txl.wanandroid.my_wanandroid.utils
+
+/**
+ *
+ * 项目名称: WanAndroid
+ * 类描述:
+ * 创建人: 田晓龙
+ * 创建时间: 2018/4/6 0006 19:53
+ * 修改人:
+ * 修改内容:
+ * 修改时间:
+ */
+
+
+object UrlUtils {
+}
