83a7506da4155159512b4b3837b2f1fec699bb6f
FromChainStudent <1379587022@qq.com>
2018-04-07 01:27:26 +0800

    首页

---
+++ app/src/main/java/com/txl/wanandroid/my_wanandroid/activity/HomeActivity.kt
@@ -0,0 +1,21 @@
+package com.txl.wanandroid.my_wanandroid.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.txl.wanandroid.my_wanandroid.R
+import com.txl.wanandroid.my_wanandroid.base.BaseActivity
+
+class HomeActivity : BaseActivity() {
+    override fun getLayoutId(): Int {
+        return R.layout.activity_home
+    }
+
+    override fun initView() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun loadData() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
